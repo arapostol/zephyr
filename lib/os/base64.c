fb7506712c0bb77f5c324f359c1767a54fe46bd8
aris <arapostol@gmail.com>
2021-06-11 12:48:07 +0300

    base64: change default encoding map to base64url

--- lib/os/base64.c
+++ lib/os/base64.c
@@ -33,7 +33,7 @@ static const uint8_t base64_enc_map[64] = {
 	'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 	'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
 	'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
-	'8', '9', '+', '/'
+	'8', '9', '-', '_'
 };
 
 static const uint8_t base64_dec_map[128] = {
